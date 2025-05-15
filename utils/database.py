724c0284f83d83b144f0a2c28f8b644127cf523d
11PRIMUS <alokpal2803@gmail.com>
2025-05-16 02:42:32 +0530

    lil change

--- utils/database.py
+++ utils/database.py
@@ -2,6 +2,7 @@ from langchain.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.embeddings.base import Embeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter 
+from langchain.docstore.document import Document as LangchainDocument
 
 
 #chroma vector using openai embedding model
@@ -16,7 +17,7 @@ def add_documnent(vector_store:Chroma,doc_id:str,text_pages:list):
         page_num=page_item["page"]
         page_content=page_item["content"]
         chunks=text_splitter.split_text(page_content)
-        for i, chunk_text in enumerate(chunks)
+        for i, chunk_text in enumerate(chunks):
             metadata={"source":doc_id,"page":page_num,"chunk_in_page":i} #index of chunk
             all_chunks.append(LangchainDocument(page_content=chunk_text,metadata=metadata))
     
@@ -25,17 +26,19 @@ def add_documnent(vector_store:Chroma,doc_id:str,text_pages:list):
         vector_store.persist() #changes  saved
 
  #search interface for one doc   
-def get_retriever_for_doc(vector_store:Chroma,doc_id:str,k_Results=3):
+def get_retriever_for_doc(vector_store:Chroma,doc_id:str,k_results=3):
     return vector_store.as_retriever(search_kwargs={"k":k_results,"filter":{"source":doc_id}})
 
 def get_retriever_for_all_doc(vector_store:Chroma,k_results=5): #retereiver across all doc in vector_store
     return vector_store.as_retriver(search_kwargs={"k":k_results})
 
+#extract ids from vectordb
 def get_all_doc_ids(vector_store:Chroma)-> list:
     try:
+        #if chroma works fine that good otherwise i am going with Qdrant 
         results=vector_store.get(include=["metadatas"])
         if results and results['metadatas']:
-            return list(Set(meta['source'] for meta in results['metadatas'] if 'source' in meta))
+            return list(set(meta['source'] for meta in results['metadatas'] if 'source' in meta))
     except Exception as e:
         print(f"error getting all doc ids:{e}")
     return []
