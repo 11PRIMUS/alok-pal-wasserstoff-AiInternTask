7c034d472b99982f1a845b8ade11c4ea4793bf9a
11PRIMUS <alokpal2803@gmail.com>
2025-05-15 23:03:55 +0530

    for docx files

--- utils/ocr_doc.py
+++ utils/ocr_doc.py
@@ -7,8 +7,15 @@ def get_doc(file_path:Path) ->list:
     pages_count=[]
     ext=file_path.suufix.lower()
 
-    if ext == ".pdf":
+    if ext == ".pdf": # for pdfs
         doc=fitz.open(file_path)
         for page_num, page in enumerate(doc):
             pages_content.append({"page":page_num+1,"content":page.get_text()})
             doc.close()
+    
+    elif ext==".docx": #for docx files 
+        doc=Document(file_path)
+        full_text=[]
+        for para in doc.paragraphs:
+            full_text.append(para.text)
+        pages_content.append({"page":1,"content":"\n".join(full_text)})
